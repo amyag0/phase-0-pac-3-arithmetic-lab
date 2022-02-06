f1e3894edc495ce5bec2b0fe97857315b04cde32
Amy <amyago19@gmail.com>
2022-02-06 17:24:05 -0500

    commit tree 9ce6695f8dd92902cac8ec5db324fce906d4cb47

---
+++ index.js
@@ -0,0 +1,35 @@
+var a = Math.floor(Math.random() * 1000);
+var b = Math.floor(Math.random() * 1000);
+
+function add(a,b){
+    return (a+b);
+}
+
+function subtract(a,b){
+    return (a-b);
+}
+
+function multiply(a,b){
+    return (a*b);
+}
+
+function divide(a,b){
+    return (a/b)
+}
+
+function increment(a){
+    return (a+1);
+}
+
+function decrement(a){
+    return (a-1);
+}
+
+function makeInt(a){
+    return parseInt(a,10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+consolelog(preserveDecimal(2.222));
